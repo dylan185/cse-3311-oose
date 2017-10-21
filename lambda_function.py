92ae037eed1c2406015d501aad2247af0417d24d
Kashif Iqbal <kashifiqbal@kashifs-mbp>
2017-10-21 18:56:35 -0500

    final - making use of tmp directory in aws

--- lambda_function.py
+++ lambda_function.py
@@ -11,40 +11,128 @@ from bs4 import BeautifulSoup
 #cur = conn.cursor()
 temp_string = ''
 contents = []
-
-file = open("tmp/handler.txt", "r")
-data = file.readlines()
-articleDate = data[0][:10]
-
 now = datetime.datetime.now()
 currentDate = str(now)[:10]
 currentHour = now.hour
 
-if (articleDate != currentDate) and (currentHour > 9):
-    os.remove("tmp/handler.txt")
-    # Get WebPage
+try:
+    file = open("/tmp/handler.txt", "r")
+    data = file.readlines()
+    articleDate = data[0][:10]
+    file_check = True
+    
+
+    if (articleDate != currentDate) and (currentHour > 9):
+        os.remove("/tmp/handler.txt")
+        # Get WebPage
+        site_base = 'http://www.theshorthorn.com'
+        quote_page = 'http://www.theshorthorn.com/news/campus/'
+
+        # Parse WebPage
+        page = urllib.request.urlopen(quote_page)
+        soup = BeautifulSoup(page, 'html.parser')
+
+        # Find Top Articles from Page
+        top_articles = soup.find('div', attrs={'id': 'tncms-region-index-primary'})
+
+        # Get href links to articles and store in a list
+        links = [a['href'] for a in top_articles.find_all('a', href=True) if a.text.strip()]
+
+        # Deletes the comment links for each article (every odd position)
+        del links[1::2]
+
+        # Declare whitelist, i &lists
+        i = 0
+        headlines = []
+        full_links = []
+        VALID_TAGS = ['p']
+
+        breaker = 1
+        # Piece together links
+        for link in links:
+            temp = site_base + link
+            if temp[:-9] not in full_links:
+                full_links.append(temp)
+            #print(temp)
+            else:
+                breaker = breaker - 1
+            if breaker == 10:
+                break
+            else:
+                breaker = breaker + 1
+
+        breaker = 1
+
+        # Goes to each link and gets headline and content
+
+        for link in full_links:
+            page = urllib.request.urlopen(link)
+            soup = BeautifulSoup(page, 'html.parser')
+            headline = soup.find('h1', attrs={'class': 'headline'})
+            headlines.append(headline.text.strip())
+            
+            paragraphs = soup.find('div', attrs={'class': 'asset-content subscriber-premium'})
+            p = paragraphs.find_all('p')
+            tempstring = ''
+            
+            for item in p:
+                tempstring = tempstring + ' ' + item.get_text(strip=True)
+            
+            contents.append(tempstring)
+            if breaker == 10:
+                break
+            else:
+                breaker = breaker + 1
+
+        # Combine into 2D list
+        article = [headlines, contents]
+
+
+        for j in range(0, len(article[0])):
+            if j != 0:
+                temp_string = temp_string + ' . . ' + article[0][j]
+            else:
+                temp_string = temp_string + article[0][j]
+
+        file = open('/tmp/handler.txt','w+')
+        file.write(currentDate)
+        file.write('\n')
+        file.write(temp_string)
+        file.write('\n')
+        j = 0
+        for j in range(0, len(article[1])):
+            file.write(article[1][j])
+            file.write('\n')
+
+        file.close()
+    else:
+        i = 0
+        for i in range(0, 10):
+            contents.append(data[i+2])
+        temp_string = data[1]
+except:
     site_base = 'http://www.theshorthorn.com'
     quote_page = 'http://www.theshorthorn.com/news/campus/'
-
+    
     # Parse WebPage
     page = urllib.request.urlopen(quote_page)
     soup = BeautifulSoup(page, 'html.parser')
-
+    
     # Find Top Articles from Page
     top_articles = soup.find('div', attrs={'id': 'tncms-region-index-primary'})
-
+    
     # Get href links to articles and store in a list
     links = [a['href'] for a in top_articles.find_all('a', href=True) if a.text.strip()]
-
+    
     # Deletes the comment links for each article (every odd position)
     del links[1::2]
-
+    
     # Declare whitelist, i &lists
     i = 0
     headlines = []
     full_links = []
     VALID_TAGS = ['p']
-
+    
     breaker = 1
     # Piece together links
     for link in links:
@@ -60,9 +148,9 @@ if (articleDate != currentDate) and (currentHour > 9):
             breaker = breaker + 1
 
     breaker = 1
-
+    
     # Goes to each link and gets headline and content
-
+    
     for link in full_links:
         page = urllib.request.urlopen(link)
         soup = BeautifulSoup(page, 'html.parser')
@@ -81,7 +169,7 @@ if (articleDate != currentDate) and (currentHour > 9):
             break
         else:
             breaker = breaker + 1
-
+    
     # Combine into 2D list
     article = [headlines, contents]
 
@@ -92,7 +180,7 @@ if (articleDate != currentDate) and (currentHour > 9):
         else:
             temp_string = temp_string + article[0][j]
 
-    file = open('tmp/handler.txt','w')
+    file = open('/tmp/handler.txt','w+')
     file.write(currentDate)
     file.write('\n')
     file.write(temp_string)
@@ -102,13 +190,7 @@ if (articleDate != currentDate) and (currentHour > 9):
         file.write(article[1][j])
         file.write('\n')
 
-    file.close()
-else:
-    i = 0
-    for i in range(0, 10):
-        contents.append(data[i+2])
-    temp_string = data[1]
-
+file.close()
 #This is the lambda function, the event parameter is the Jason request from which we will extract the intents.
 def lambda_handler(event, context):
     # This is to check to make sure our app is the only skill that can access this lambda function
